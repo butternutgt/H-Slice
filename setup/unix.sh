d8ab9d2c6a969203418ab347285f394cb08cb958
HRK.EXEX <74143200+HRK-EXEX@users.noreply.github.com>
2025-05-18 00:49:00 +0900

    trying to change repo to git in linux build... lemme try

--- setup/unix.sh
+++ setup/unix.sh
@@ -7,7 +7,7 @@ echo Makking the main haxelib and setuping folder in same time..
 mkdir ~/haxelib && haxelib setup ~/haxelib
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib install lime 8.2.2 --quiet
+
 haxelib install openfl 9.4.1 --quiet
 haxelib install flixel-addons 3.3.2 --quiet
 haxelib install flixel-tools 1.5.1 --quiet
@@ -15,7 +15,10 @@ haxelib install hscript-iris 1.1.3 --quiet
 haxelib install tjson 1.4.0 --quiet
 haxelib install hxdiscord_rpc 1.2.4 --quiet
 haxelib install flixel 6.0.0 --quiet
-haxelib set flixel 6.0.0
+
+haxelib git hxcpp https://github.com/Psych-Slice/hxcpp-mobile.git  --quiet
+haxelib git lime https://github.com/Psych-Slice/lime-mobile.git --quiet 608f3f7196b2b58df19afff4aedec5bdda107301
+
 haxelib git flxanimate https://github.com/Psych-Slice/FlxAnimate.git 42f1b5d193b4345ca7d6933380ab3105985b44a3 --quiet
 haxelib git hxCodec https://github.com/polybiusproxy/hxCodec.git 0a51aed0d9523d22a83e453ce7b593ec7fed4742 --quiet
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7 --quiet
